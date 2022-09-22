0004ac2e605e1f5b91e1d11491ac27277e689b9e
modengann <99417989+modengann@users.noreply.github.com>
2022-09-22 10:12:32 -0400

    init

---
+++ src/cycling_weather.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def split_date():
+    pass
+
+def split_date_continues():
+    pass
+
+def cycling_weather():
+    pass
+
+def main():
+    cycling_weather()
+
+if __name__ == "__main__":
+    main()
